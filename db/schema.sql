137b7b266dfa2d6d4c15875f1b27489b8778277b
Robbie K <rkahl08@gmail.com>
2022-07-04 10:23:14 -0500

    schema update

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+USE employee_db;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
+-- departemnt table
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
+);
+
+-- role table
+Create TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    role_title VARCHAR(30) NOT NULL,
+    role_salary DECIMAL NOT NULL,
+    departemnt_id INTEGER,
+    PRIMARY KEY (id)
+);
